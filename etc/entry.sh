4228f7c0882f6154c77905d72d52d0949a043fd9
Matthew <matteus12122@gmail.com>
2023-12-20 17:32:01 +0100

    Update entry.sh

--- etc/entry.sh
+++ etc/entry.sh
@@ -66,6 +66,7 @@ fi
 bash "${STEAMAPPDIR}/srcds_run" -game "${STEAMAPP}" -console \
                         -steam_dir "${STEAMCMDDIR}" \
                         -steamcmd_script "${HOMEDIR}/${STEAMAPP}_update.txt" \
+			-usercon \
                         +fps_max "${SRCDS_FPSMAX}" \
                         -tickrate "${SRCDS_TICKRATE}" \
                         +port "${SRCDS_PORT}" \
